a024ba9ec579679e0d5b0ecfb0134990d9963bfc
sonamonachan-57 <sonamonachan57@gmail.com>
2026-02-27 09:42:54 +0530

    Add files via upload

---
+++ text_to_text.py
@@ -0,0 +1,77 @@
+import os
+import sys
+venv_path = '/home/sona-inc5619/mp_env/lib/python3.12/site-packages'
+if os.path.exists(venv_path) and venv_path not in sys.path:
+    sys.path.insert(0, venv_path)
+
+
+import ollama
+
+#load product manual
+
+with open("product_manual.txt","r",encoding="utf-8") as f:
+    manual_text=f.read()
+    manual_sections=manual_text.split("\n\n")
+    
+#simple retrieval function
+
+def retrieve_answer(query):
+    query=query.lower()
+    for section in manual_sections:
+        if any(word in section.lower() for word in query.split()):
+            return section
+        return None
+
+
+history=[]
+model="llama3"
+
+
+
+while True:
+     prompt=input("you: ").strip()
+     if prompt.lower() in ('exit', 'quit'):
+         print("have a nice day")
+         break
+    #retrieve from manual
+     retrieved_section=retrieve_answer(prompt)
+     if retrieved_section:
+         
+        advanced_prompt=f"""
+                        you are a  regional customer support assistant.Answer clearly using the product information.
+                         product manual info:{retrieved_section}
+                      user question:{prompt}
+                        """
+     else:
+         advanced_prompt=f"""
+                         you are a customer support assistant. If the answer is not in the product manual.say:"I am transferring you to a human support agent."
+                         user Question:
+                         {prompt}
+                         """
+     #add to history
+     message={
+         "role":"user",
+        
+         "content": advanced_prompt
+     }
+     history.append(message)
+     #ollama bot
+     response=ollama.chat(model=model,messages=history)
+     bot_message_content=response["message"]["content"]  
+     print(f"Bot: {bot_message_content}")
+     
+     #store assistant reply
+     
+     bot_message={
+         "role":"assistant",
+         "content": bot_message_content
+     }
+     history.append(bot_message)
+       
+    
+    
+
+   
+ 
+      
+   
